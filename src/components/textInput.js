a4203d7d2743afc4fe545e3702455fb062c4abd8
Ryz3D <mirco.heitmann@gmail.com>
2022-03-23 11:11:42 +0100

    ideas

--- src/components/textInput.js
+++ src/components/textInput.js
@@ -1,6 +1,25 @@
 import * as mui from '@mui/material';
 import React from 'react';
 
+/*
+
+beispiel profilseite
+ -> textinputs edit oder display mode prop
+ -> image upload
+ -> save profile data in localstorage
+
+ -> class
+ -> constructor
+ -> didmount
+ -> state
+ -> props
+ -> data object (in state)
+ -> JSON
+ -> localstorage
+ -> image base64, blob
+
+*/
+
 class TextInputComponent extends React.Component {
   render() {
     return (
